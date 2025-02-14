77cd1b1c8f6e10d8aba20b1c36afc7f4ad583bd1
emmaklehto <emmaklehto@gmail.com>
2025-02-14 11:14:45 +0200

    Update WeatherDataRetriever.java

--- app/src/main/java/com/example/harkkatyo/WeatherDataRetriever.java
+++ app/src/main/java/com/example/harkkatyo/WeatherDataRetriever.java
@@ -9,7 +9,7 @@ import java.io.IOException;
 import java.net.URL;
 
 public class WeatherDataRetriever {
-    private final String API_KEY = "b7770b753da7a9f17a1d9ea4e90719f7";
+    private final String API_KEY = "55518035dc0960a48a61c2d2bd05b910";
     private final String CONVERTER_BASE_URL = "https://api.openweathermap.org/geo/1.0/direct?q=%s&limit=5&appid=%s";
     private final String WEATHER_BASE_URL = "https://api.openweathermap.org/data/2.5/weather?lat=%s&lon=%s&lang=fi&units=metric&appid=%s";
 
